eba8252c181f05436b051d57e6e2eda438870ebe
Shirin <shirin.mohajer@gmail.com>
2023-10-04 16:16:00 -0400

    save update

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.listen(PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`));
+
+app.get(`/greet/:name`, (req, res) => {
+    console.log('hello There!');
+    res.send(`hello there, ${req.params.name} `);
+
+});
