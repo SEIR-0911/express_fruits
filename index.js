81dceda3ea15602ee8d357f37151bbd46a581746
Shirin <shirin.mohajer@gmail.com>
2023-10-06 13:33:59 -0400

    working on index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
 app.listen(PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`));
 
-app.get(`/greet/:name`, (req, res) => {
-    console.log('hello There!');
-    res.send(`hello there, ${req.params.name} `);
+app.get("/", (req, res) => {
+    res.send("Hello! Welcome to Frute APP.");
+});
+
+app.get(`/greet/:username`, (req, res) => {
+    console.log(`hello ${req.params.username}!`);
+    res.send(`hello ${req.params.username}!`);
 
 });
+//GET /five should return an array of the numbers from 1-5
+const myNum =[1,2,3,4,5]
+
+app.get('/five', (req, res) => {
+    console.log("",);
+    res.send(`hello ${req.params}!`);
+}) 
+    
+
+
+app.get('/fruits/', (req, res) => {
+    //your code here
+    // HINT - you can use a higher-order array method 
+    console.log(`hello ${req.params}!`);
+    res.send(`hello ${req.params}!`);
+})
+  
